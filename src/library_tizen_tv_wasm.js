0d900b332e1592b1c1edec82c0da35409bedfb98
Piotr Bałut <p.balut@samsung.com>
2020-10-07 14:31:28 +0200

    Updated Tizen TV WASM versioning scheme.
    * Updated TizenTVApiInfo structure.
    * Added support for Tizen TV API features testing via
      samsung::wasm::IsApiFeatureSupported(api, feature).
    
    Bug: https://cam.sprc.samsung.pl/browse/VDWASM-412
    Signed-off-by: Piotr Bałut <p.balut@samsung.com>
    Change-Id: I47e92ee6ccc42a5bccbfc704649d8a56640e6660

--- src/library_tizen_tv_wasm.js
+++ src/library_tizen_tv_wasm.js
@@ -23,13 +23,15 @@ const LibraryTizenTVWasm = {
           return {
             name: 'ElementaryMediaStreamSource',
             version: '0.9',
-            apiLevels: [1]
+            apiLevels: [1],
+            features: ['base-emss'],
           };
         } else {
           return {
             name: 'ElementaryMediaStreamSource',
             version: '0.1',
-            apiLevels: [0]
+            apiLevels: [0],
+            features: ['legacy-emss'],
           };
         }
       }
@@ -40,14 +42,58 @@ const LibraryTizenTVWasm = {
       return {
         name: 'TizenSockets',
         version: '1.0',
-        apiLevels: [1]
+        apiLevels: [1],
+        features: [],
       };
+    },
+
+    getLegacyApis: function() {
+      const legacyApis = [
+        TIZENTVWASM.getLegacyWasmSocketsVersion(),
+      ];
+      const legacyEmss = TIZENTVWASM.getLegacyWasmPlayerVersion();
+      if (legacyEmss) {
+        legacyApis.push(legacyEmss);
+      }
+      return legacyApis;
+    },
+
+    getAvailableApis: function() {
+      const hasAvailableApis =
+          (typeof tizentvwasm.availableApis !== 'undefined');
+      const hasApiFeatureSupported =
+          (typeof tizentvwasm.isApiFeatureSupported !== 'undefined');
+
+      const availableApis = hasAvailableApis ?
+        tizentvwasm.availableApis : TIZENTVWASM.getLegacyApis();
+
+      // Polyfill `features` key if we're on platform it's not supported:
+      if (hasAvailableApis && !hasApiFeatureSupported) {
+        availableApis.forEach((apiInfo) => {
+          if (apiInfo.name === 'ElementaryMediaStreamSource') {
+            apiInfo.features = ['base-emss'];
+            if (apiInfo.apiLevels.includes(2)) {
+              apiInfo.features.push('video-texture');
+            }
+            if (apiInfo.apiLevels.includes(3)) {
+              apiInfo.features.push('software-decoding');
+            }
+            return;
+          }
+          // Only WASM Player had any features that could be emulated before
+          // isApiFeatureSupported() API was added.
+          apiInfo.features = [];
+        });
+      }
+
+      return availableApis;
     },
   },
 
   TizenTVWasm_GetAvailableApis__deps: ['$TIZENTVWASM'],
-  TizenTVWasm_GetAvailableApis__sig: 'vii',
-  TizenTVWasm_GetAvailableApis: function(callback, userData) {
+  TizenTVWasm_GetAvailableApis__sig: 'viiii',
+  TizenTVWasm_GetAvailableApis: function(
+    addFeatureCb, addApiInfoCb, featuresPtr, apiInfoPtr) {
     if (!TIZENTVWASM.hasTizenTVWasm()) {
       console.error('Not a TizenTV device?');
       return;
@@ -56,15 +102,29 @@ const LibraryTizenTVWasm = {
     const addResult = function(apiInfo) {
       if (!apiInfo) return;
 
+      // Populate features
+      apiInfo.features.forEach((feature) => {
+        const featurePtr = TIZENTVWASM.allocCStr(feature);
+        try {
+          dynCall('vii', addFeatureCb, [featurePtr, featuresPtr]);
+        } catch (error) {
+          console.error(error);
+        } finally {
+          _free(featurePtr);
+        }
+      });
+
+      // Populate api info
       const namePtr = TIZENTVWASM.allocCStr(apiInfo.name);
       const versionPtr = TIZENTVWASM.allocCStr(apiInfo.version);
       const levelsPtr = _malloc(apiInfo.apiLevels.byteLength);
       try {
-        for (var i = 0; i != apiInfo.apiLevels.length; i++) {
+        for (let i = 0; i != apiInfo.apiLevels.length; i++) {
           setValue(levelsPtr + (i * 4), apiInfo.apiLevels[i], 'i32');
         }
-        dynCall('viiiii', callback, [
-          namePtr, versionPtr, levelsPtr, apiInfo.apiLevels.length, userData
+        dynCall('viiiiii', addApiInfoCb, [
+          namePtr, versionPtr, levelsPtr, apiInfo.apiLevels.length,
+          featuresPtr, apiInfoPtr,
         ]);
       } catch (error) {
         console.error(error);
@@ -75,12 +135,7 @@ const LibraryTizenTVWasm = {
       }
     };
 
-    if (tizentvwasm.availableApis === undefined) {
-      addResult(TIZENTVWASM.getLegacyWasmPlayerVersion());
-      addResult(TIZENTVWASM.getLegacyWasmSocketsVersion());
-    } else {
-      tizentvwasm.availableApis.forEach(addResult);
-    }
+    TIZENTVWASM.getAvailableApis().forEach(addResult);
   },
 
   TizenTVWasm_GetSupportedInstructions__deps: ['$TIZENTVWASM'],
@@ -110,17 +165,10 @@ const LibraryTizenTVWasm = {
       return 0;
     }
     const apiName = UTF8ToString(apiNamePtr);
-    if (tizentvwasm.isApiSupported) {
+    if (typeof tizentvwasm.isApiSupported !== 'undefined') {
       return (tizentvwasm.isApiSupported(apiName, apiLevel) ? 1 : 0);
     } else {
-      const legacyApis = [];
-      if (TIZENTVWASM.getLegacyWasmPlayerVersion()) {
-        legacyApis.push(TIZENTVWASM.getLegacyWasmPlayerVersion());
-      }
-      if (TIZENTVWASM.getLegacyWasmSocketsVersion()) {
-        legacyApis.push(TIZENTVWASM.getLegacyWasmSocketsVersion());
-      }
-
+      const legacyApis = TIZENTVWASM.getAvailableApis();
       for (const api of legacyApis) {
         if (api.name == apiName && api.apiLevels.includes(apiLevel)) {
           return 1;
@@ -130,6 +178,27 @@ const LibraryTizenTVWasm = {
     }
   },
 
+  TizenTVWasm_IsApiFeatureSupported__deps: ['$TIZENTVWASM'],
+  TizenTVWasm_IsApiFeatureSupported__sig: 'iii',
+  TizenTVWasm_IsApiFeatureSupported: function(apiNamePtr, featurePtr) {
+    if (!TIZENTVWASM.hasTizenTVWasm()) {
+      console.error('Not a TizenTV device?');
+      return 0;
+    }
+    const apiName = UTF8ToString(apiNamePtr);
+    const feature = UTF8ToString(featurePtr);
+    if (typeof tizentvwasm.isApiFeatureSupported !== 'undefined') {
+      return (tizentvwasm.isApiFeatureSupported(apiName, feature) ? 1 : 0);
+    } else {
+      const legacyApis = TIZENTVWASM.getAvailableApis();
+      for (const api of legacyApis) {
+        if (api.name == apiName && api.features.includes(feature)) {
+          return 1;
+        }
+      }
+      return 0;
+    }
+  },
 };
 
 mergeInto(LibraryManager.library, LibraryTizenTVWasm);
--- system/lib/samsung/wasm/tizen_tv_wasm.cc
+++ system/lib/samsung/wasm/tizen_tv_wasm.cc
@@ -6,13 +6,20 @@
 #include "samsung/wasm/tizen_tv_wasm.h"
 
 extern "C" {
-typedef void TizenTVWasm_AddApiFn(const char* name,
-                                  const char* version,
-                                  const uint32_t* api_levels,
-                                  uint32_t api_level_size,
-                                  void* user_data);
-extern void TizenTVWasm_GetAvailableApis(TizenTVWasm_AddApiFn* add_api_fn,
-                                         void* user_data);
+typedef void TizenTVWasm_AddFeatureFn(const char* feature,
+                                      std::vector<std::string>* features_ptr);
+typedef void TizenTVWasm_AddApiFn(
+    const char* name,
+    const char* version,
+    const uint32_t* api_levels,
+    uint32_t api_level_size,
+    std::vector<std::string>* features_ptr,
+    std::vector<samsung::wasm::TizenTVApiInfo>* api_infos_ptr);
+extern void TizenTVWasm_GetAvailableApis(
+    TizenTVWasm_AddFeatureFn* add_feature_fn,
+    TizenTVWasm_AddApiFn* add_api_fn,
+    std::vector<std::string>* features_ptr,
+    std::vector<samsung::wasm::TizenTVApiInfo>* api_info_ptr);
 
 typedef void TizenTVWasm_AddInstructionsFn(const char* name, void* user_data);
 extern void TizenTVWasm_GetSupportedInstructions(
@@ -20,6 +27,8 @@ extern void TizenTVWasm_GetSupportedInstructions(
     void* user_data);
 
 extern int TizenTVWasm_IsApiSupported(const char* name, uint32_t api_level);
+extern int TizenTVWasm_IsApiFeatureSupported(const char* name,
+                                             const char* feature);
 }
 
 namespace samsung {
@@ -27,16 +36,21 @@ namespace wasm {
 
 std::vector<TizenTVApiInfo> GetAvailableApis() {
   std::vector<TizenTVApiInfo> result;
+  std::vector<std::string> features_temp_storage;
   // only a lambda with no capture is convertible to a function pointer
   TizenTVWasm_GetAvailableApis(
-      [](const char* name, const char* version, const uint32_t* api_levels,
-         uint32_t api_level_size, void* user_data) {
-        auto* result = static_cast<std::vector<TizenTVApiInfo>*>(user_data);
-        result->push_back(TizenTVApiInfo{
-            name, version,
-            std::vector<uint32_t>{api_levels, api_levels + api_level_size}});
+      [](const char* feature_str, std::vector<std::string>* features_ptr) {
+        features_ptr->push_back(feature_str);
       },
-      &result);
+      [](const char* name, const char* version, const uint32_t* api_levels,
+         uint32_t api_level_size, std::vector<std::string>* features_ptr,
+         std::vector<TizenTVApiInfo>* api_infos_ptr) {
+        api_infos_ptr->push_back(TizenTVApiInfo{
+            name, version,
+            std::vector<uint32_t>{api_levels, api_levels + api_level_size},
+            std::move(*features_ptr)});
+      },
+      &features_temp_storage, &result);
   return result;
 }
 
@@ -54,6 +68,11 @@ std::vector<std::string> GetSupportedInstructions() {
 
 bool IsApiSupported(const std::string& api_name, uint32_t api_level) {
   return !!TizenTVWasm_IsApiSupported(api_name.c_str(), api_level);
+}
+
+bool IsApiFeatureSupported(const std::string& api_name,
+                           const std::string& feature) {
+  return !!TizenTVWasm_IsApiFeatureSupported(api_name.c_str(), feature.c_str());
 }
 
 }  // namespace wasm
